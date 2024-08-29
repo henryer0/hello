5831f1bf54f18531bbead884b0f29c53fe4f49b8
henryer0 <henryle223@gmail.com>
2024-08-29 12:45:29 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Henry Le
+ *
+ * Date: 2024/8/29
+ *
+ * This code prints my name
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (int argc, char **argv) {
+
+    printf("Hello my name is Henry Le");
+
+    return 0;
+
+}
