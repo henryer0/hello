4135d249a1fbcefa8ed843df2230c0ca92a17ef9
Your Name <halyassiri2@huskers.edu.unl>
2024-08-29 13:48:31 -0500

    added my major and name

--- hello.c
+++ hello.c
@@ -12,8 +12,11 @@
 int main (int argc, char **argv) {
 
     printf("Hello my name is Henry Le!");
+    printf("Hello my name is Hassanen Alyassiri");
+
 
     printf("My major is Computer Science");
+     printf("My major is Computer Science");
 
     return 0;
 
